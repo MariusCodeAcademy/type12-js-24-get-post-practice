63e75a2a217347357b05a43fdc3d100f6c26b44a
Marius <marius@example.com>
2022-08-09 09:12:43 +0300

    helper

--- js/helper.js
+++ js/helper.js
@@ -2,3 +2,12 @@
 console.log('helper.js');
 
 const baseUrl = 'https://dummyjson.com/posts';
+
+async function initHeader() {
+  // paimti layout/header.html
+  const resp = await fetch('layout/header.html');
+  const htmlHeader = await resp.text();
+  // ideti i body pradzia
+  document.body.insertAdjacentHTML('afterbegin', htmlHeader);
+}
+initHeader();
